e1614292f65d26e26ce32c12a4fb18f92ae27314
CrAsHeR034 <128222450+CrAsHeR034@users.noreply.github.com>
2023-09-21 12:59:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix || matrix.length === 0) {
+        return [];
+    }
+
+    const sortedArray = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            // Even row, left to right
+            for (let j = 0; j < matrix[i].length; j++) {
+                sortedArray.push(matrix[i][j]);
+            }
+        } else {
+            // Odd row, right to left
+            for (let j = matrix[i].length - 1; j >= 0; j--) {
+                sortedArray.push(matrix[i][j]);
+            }
+        }
+    }
+    return sortedArray;
 }
